1400063d2677787dad79e7fb662aa45f863a8e4c
Don Abney <tdonaldabney@gmail.com>
2017-03-17 15:32:09 +0000

    setting the different board states

--- models/boardState.js
+++ models/boardState.js
@@ -1,7 +1,15 @@
 var mraa = require('mraa');
 var BoardState = function() {};
 
-BoardState.prototype.setBoardState = function(req) {
+var boardState = {};
+
+BoardState.prototype.initBoardState = function() {
+  boardState['greenPin'] = {pin: 11, value: 0};
+  boardState['yellowPin'] = {pin: 12, value: 0};
+  boardState['redPin'] = {pin: 13, value: 0};    
+};
+
+BoardState.prototype.setBoardState = function(state) {
 
   //parse the correct status
   //set the new board state
@@ -9,11 +17,21 @@ BoardState.prototype.setBoardState = function(req) {
   //var pin8 = new mraa.Gpio(8);
   //pin8.dir(mraa.DIR_OUT);
   //pin8.write(1);
-  return {
-    greenPin: {value: 0},
-    yellowPin: {value: 0},
-    redPin: {value: 1}
+  this.initBoardState();
+
+  switch (state) {
+    case 'success':
+      boardState['greenPin'].value = 1;
+      break;
+    case 'warning':
+      boardState['yellowPin'].value = 1;
+      break;
+    case 'failure':
+      boardState['redPin'].value = 1;
+      break;
   }
+
+  return boardState;
 };
 
 module.exports = BoardState;
--- tests/models/boardState_spec.js
+++ tests/models/boardState_spec.js
@@ -12,7 +12,28 @@ describe('BoardState: ', function() {
       expect(actualResponse.greenPin.value).toEqual(0);
       expect(actualResponse.yellowPin.value).toEqual(0);
       expect(actualResponse.redPin.value).toEqual(1);
+    })
 
+    it('sets the pin for the green light when success', function() {
+      var boardState = new BoardState();
+      var state = 'success';
+
+      var actualResponse = boardState.setBoardState(state);
+
+      expect(actualResponse.greenPin.value).toEqual(1);
+      expect(actualResponse.yellowPin.value).toEqual(0);
+      expect(actualResponse.redPin.value).toEqual(0);
+    })
+
+    it('sets the pin for the yellow light when warning', function() {
+      var boardState = new BoardState();
+      var state = 'warning';
+
+      var actualResponse = boardState.setBoardState(state);
+
+      expect(actualResponse.greenPin.value).toEqual(0);
+      expect(actualResponse.yellowPin.value).toEqual(1);
+      expect(actualResponse.redPin.value).toEqual(0);
     })
   })
 })
